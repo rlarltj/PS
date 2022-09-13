0528f63989a7b297c8f26a7f936229909afbfcac
kiseo <5rne@naver.com>
2022-09-13 09:14:06 +0900

    dp

--- src/com/company/dp/No1010.java
+++ src/com/company/dp/No1010.java
@@ -1,36 +1,48 @@
-//package com.company.dp;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//public class No1010 {
-//    static int N;
-//    static int
-//    public static void main(String[] args) throws IOException {
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int num = Integer.parseInt(br.readLine());
-//        StringBuffer sb = new StringBuffer();
-//
-//    }
-//
-//    static class a{
-//        public static void main(String[] args) {
-//            Scanner sc= new Scanner(System.in);
-//            int n = sc.nextInt();
-//            int[] arr = new int[n];
-//            for (int i = 0; i < n; i++) {
-//                arr[i] = sc.nextInt();
-//            }
-//
-//            Arrays.sort(arr);
-//
-//            for (int i = n - 1; i >= 0; i--) {
-//                System.out.println(arr[i]);
-//            }
-//        }
-//    }
-//}
+package com.company.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class No1010 {
+    static int T;
+    static int[][] dp = new int[30][30];
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            int result = solve(M, N);
+            sb.append(result).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int solve(int n, int r) {
+
+        // 이미 풀린 경우 바로 반환
+        if(dp[n][r] > 0) {
+            return dp[n][r];
+        }
+
+        // 2번 성질
+        if(n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        // 1번 성질
+        return dp[n][r] = solve(n - 1, r - 1) + solve(n - 1, r);
+    }
+}
